a2f3ab25220a6399e58cf49650fb7e33d231c053
pighaddt <pighaddt@gmail.com>
2019-06-04 00:55:18 +0800

    1st

---
+++ src/main/kotlin/com/angus/Hello.kt
@@ -0,0 +1,16 @@
+package com.angus
+
+fun main() {
+//    println("Hello kotlin");
+//    Human().hello();
+    val h = Human();
+    h.hello();
+}
+
+class Human {
+    fun hello(){
+
+     println("Hello kotlin");
+
+    }
+}
