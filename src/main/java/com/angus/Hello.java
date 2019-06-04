4f9e534e28398b97295809717bc7cb436d61591b
pighaddt <pighaddt@gmail.com>
2019-06-04 13:57:02 +0800

    StringTokenizer

--- src/main/java/com/angus/Hello.java
+++ src/main/java/com/angus/Hello.java
@@ -1,11 +1,24 @@
 package com.angus;
 
+import java.util.StringTokenizer;
+
 public class Hello {
     public static void main(String[] args) {
 //        System.out.println("Hello world");
 
 //        new Person().hello();
-        Person p = new Person();
-        p.hello();
+//        Person p = new Person();
+//        p.hello();
+        String s = new String("\n1\n2\n3\n4\n5\n");
+//        System.out.println(s);
+        StringTokenizer st = new StringTokenizer(s,"\n");
+        int n = st.countTokens();
+        System.out.println("separate" + n +"number");
+        while(st.hasMoreTokens()){
+
+            System.out.print(st.nextToken());
+
+        }
+
     }
 }
